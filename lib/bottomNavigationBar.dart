f23332a99a5c15f97a34f4e7daaf9c6b03ef5311
bastientardits <bastien.tardits@edu.ece.fr>
2023-02-09 12:21:05 +0100

    navbar complete

--- lib/bottomNavigationBar.dart
+++ lib/bottomNavigationBar.dart
@@ -22,14 +22,20 @@ class _NavBarState extends State<NavBar> {
   Widget build(BuildContext context) {
     Widget page;
     switch (selectedIndex) {
-      case 2:
-        page = BDD();
+      case 0:
+        page = Home();
         break;
       case 1:
         page = Home();
         break;
-      case 0:
-        page = Home();
+      case 2:
+        page = BDD();
+        break;
+      case 3:
+        page = BDD();
+        break;
+      case 4:
+        page = BDD();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
@@ -38,12 +44,16 @@ class _NavBarState extends State<NavBar> {
       body:page,
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Placer un stand'),
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.data_array), label: 'BDD')
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),//tooltip pour changer le label en haut
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Rechercher un objet'),
+          BottomNavigationBarItem(icon: Icon(Icons.map), label: 'Map'), //changer en map
+          BottomNavigationBarItem(icon: Icon(Icons.archive), label: 'Placer un stand'), // intégrer placer un stand a info vendeur
+          BottomNavigationBarItem(icon: Icon(Icons.manage_accounts), label: 'Profil'),
         ],
         currentIndex: selectedIndex,
         selectedItemColor: Colors.amber,
+        unselectedItemColor: Colors.grey,
+
         onTap: _onItemTapped,
       ),
     );
