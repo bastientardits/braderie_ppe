d69fb6284cff42e200e36b26d03f1e7f24c56536
bastientardits <bastien.tardits@edu.ece.fr>
2023-02-09 12:43:27 +0100

    bdd pade display bdd content

--- lib/BDD.dart
+++ lib/BDD.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 class BDD extends StatefulWidget {
   const BDD({Key? key}) : super(key: key);
@@ -10,8 +12,25 @@ class BDD extends StatefulWidget {
 class _BDDState extends State<BDD> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text('bdd'),
+     return Scaffold(
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('stand').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return ListView(
+            children: snapshot.data!.docs.map((document) {
+              return Container(
+                child: Center(child: Text(document['address'])),
+              );
+            }).toList(),
+          );
+        },
+      ),
     );
   }
 }
