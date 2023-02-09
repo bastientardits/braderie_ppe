92d85cc4552afeebe48f8ea20e95c980afcc3e39
FARESM213 <fares.messaoudi@edu.ece.fr>
2023-02-09 02:57:50 +0100

    Base de donnes ecriture

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-
 import 'osmhome.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+ Future<void> main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+   await Firebase.initializeApp(
+     options: DefaultFirebaseOptions.currentPlatform,
+   );
+   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
