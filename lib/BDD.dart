680ee868d1ee3666704c3b34b716b46f64fb227d
bastientardits <bastien.tardits@edu.ece.fr>
2023-02-09 15:21:58 +0100

    bdd page unfinished: need to display map

--- lib/BDD.dart
+++ lib/BDD.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
+import 'package:latlong2/latlong.dart';
 
 
 class BDD extends StatefulWidget {
@@ -10,26 +13,48 @@ class BDD extends StatefulWidget {
 }
 
 class _BDDState extends State<BDD> {
+  List<DocumentSnapshot> documents = [];
+  List<LatLng> coordinates = [];
+  List<Marker> markers = [];
+  @override
+  void initState() {
+    super.initState();
+    loadDocuments();
+  }
+
+  void loadDocuments() async {
+    QuerySnapshot snapshot = await FirebaseFirestore.instance.collection('stand').get();
+    setState(() {
+      documents = snapshot.docs;
+      for(var document in documents){
+        LatLng l = LatLng(document['latitude'], document['longitude']);
+        coordinates.add(l);
+      }
+      markers=coordinates 
+        .map((l) => Marker(
+        point: l,
+        width: 60,
+        height: 60,
+        builder: (context) => Icon(
+          Icons.pin_drop,
+          size: 60,
+          color: Colors.blueAccent,
+        ),
+      ))
+          .toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-     return Scaffold(
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('stand').snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (!snapshot.hasData) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
+    return Scaffold(
+      body:FlutterMap(
+        options: MapOptions(
+                    center: LatLng(50.6371, 3.0530),
+        ),
 
-          return ListView(
-            children: snapshot.data!.docs.map((document) {
-              return Container( //aa
-                child: Center(child: Text(document['address'])),
-              );
-            }).toList(),
-          );
-        },
+
+        
       ),
     );
   }
