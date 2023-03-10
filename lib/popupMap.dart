6b3f4e7d4572cebcb90a36937d3276487fbb5172
FARESM213 <fares.messaoudi@edu.ece.fr>
2023-03-10 22:02:24 +0100

    Lecture photo ( sur tel )

--- lib/popupMap.dart
+++ lib/popupMap.dart
@@ -115,7 +115,7 @@ class Popup extends StatelessWidget {
                 if (kDebugMode) {
                   print(snapshot.data.toString());
                 }
-                if(snapshot.data!=null || snapshot.data=="")
+                if(snapshot.data!=null || snapshot.data!="")
                     return Image.network(snapshot.data.toString(),height: 100,width: 100,);
                 else
                    return Text("Photo pas possible");
